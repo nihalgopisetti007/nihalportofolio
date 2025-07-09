44dbde6dcdd2d5924ed62103c99c7ccb23878a52
nihalgopisetti007 <nihalg2837@gmail.com>
2025-07-09 09:24:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-console.log(
-  "%c🦇 Welcome to Nihal Gopisetti’s Portfolio",
-  "color: #FFD700; background: #0a0a0a; font-size: 16px; padding: 6px; font-weight: bold;"
-);
-
-// Fade out the dark overlay when page finishes loading
 window.addEventListener("load", () => {
   document.body.classList.add("loaded");
 });
